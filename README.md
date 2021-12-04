a7a20e4485fcae540018764034dc310b63235737
Barış Altop <baris.altop@semper-tech.com>
2021-12-04 07:27:34 +0300

    Final version

--- README.md
+++ README.md
@@ -1,8 +1,73 @@
 # bluetooth_connector
 
-A new flutter plugin project.
+A Flutter plugin to expose the features of Bluetooth Adapter for **Android only**.
 
-## Getting Started
+## Objective
+
+The primary purpose of this project was to communicate between flutter and paired Wear OS devices using Bluetooth.
+
+## Main Features
+- Setting up a UUID from user end.
+- Checking Bluetooth connection status and giving alerts if its off or not right.
+- Getting list of paired devices.
+- Get a particular paired device info.
+- Start Bluetooth server.
+- Start Bluetooth client.
+- Start Bluetooth client.
+- Send message to a connected device.
+- Stream for listening to received messages.
+- Stream for listening to connection status (CONNECTED, CONNECTING, CONNECTION FAILED, LISTENING, DISCONNECTED).
+
+### Getting Started
+
+For a full example please check /example folder. Here are only the most important parts of the code to illustrate how to use the library.
+
+    //Initiatinng the bluetooth adapter
+    Bluetoothadapter flutterbluetoothadapter = Bluetoothadapter();
+
+	//Listening to the connection status listener
+	String _connectionStatus = "NONE";
+    StreamSubscription _btConnectionStatusListener =flutterbluetoothadapter.connectionStatus().listen((dynamic status) {
+      setState(() {
+        _connectionStatus = status.toString();
+      });
+    });
+
+	//Listening to the recieved messages
+	String _recievedMessage;
+    StreamSubscription _btReceivedMessageListener = flutterbluetoothadapter.receiveMessages().listen((dynamic newMessage) {
+      setState(() {
+        _recievedMessage = newMessage.toString();
+      });
+    });
+
+#### Getting paired devices
+
+    List<BtDevice> devices = await flutterbluetoothadapter.getDevices();
+
+#### Getting paired device
+
+    await flutterbluetoothadapter.getDevice(deviceAddress);
+
+#### Starting BT server
+
+    await flutterbluetoothadapter.startServer();
+
+#### Starting BT client
+
+    flutterbluetoothadapter.startClient(devices.indexOf(element), true);
+
+#### Sending message
+
+    flutterbluetoothadapter.sendMessage(messageString);
+
+#### Check BT connection
+	await flutterbluetoothadapter.checkBluetooth(); //returns bool
+
+#### Set UUID
+	flutterbluetoothadapter.initBlutoothConnection(uuid);
+
+### Flutter Help
 
 This project is a starting point for a Flutter
 [plug-in package](https://flutter.dev/developing-packages/),
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,44 +5,45 @@ import 'package:flutter/services.dart';
 import 'package:bluetooth_connector/bluetooth_connector.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  BluetoothConnector flutterbluetoothadapter = BluetoothConnector();
+  StreamSubscription? _btConnectionStatusListener, _btReceivedMessageListener;
+  String _connectionStatus = "NONE";
+  List<BtDevice> devices = [];
+  String? _recievedMessage;
+  TextEditingController _controller = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    flutterbluetoothadapter
+        .initBlutoothConnection("20585adb-d260-445e-934b-032a2c8b2e14");
+    flutterbluetoothadapter
+        .checkBluetooth()
+        .then((value) => print(value.toString()));
+    _startListening();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion = await BluetoothConnector.platformVersion ??
-          'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
+  _startListening() {
+    _btConnectionStatusListener =
+        flutterbluetoothadapter.connectionStatus().listen((dynamic status) {
+      setState(() {
+        _connectionStatus = status.toString();
+      });
+    });
+    _btReceivedMessageListener =
+        flutterbluetoothadapter.receiveMessages().listen((dynamic newMessage) {
+      setState(() {
+        _recievedMessage = newMessage.toString();
+      });
     });
   }
 
@@ -53,10 +54,119 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Flexible(
+                  fit: FlexFit.tight,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlinedButton(
+                      onPressed: () async {
+                        await flutterbluetoothadapter.startServer();
+                      },
+                      child: const Text('LISTEN'),
+                    ),
+                  ),
+                ),
+                Flexible(
+                  fit: FlexFit.tight,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlinedButton(
+                      onPressed: () async {
+                        devices = await flutterbluetoothadapter.getDevices();
+                        setState(() {});
+                      },
+                      child: const Text('LIST DEVICES'),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Text("STATUS - $_connectionStatus"),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0,
+                vertical: 20,
+              ),
+              child: ListView(
+                shrinkWrap: true,
+                children: _createDevices(),
+              ),
+            ),
+            Text(
+              _recievedMessage ?? "NO MESSAGE",
+              style: const TextStyle(fontSize: 24),
+            ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                  flex: 4,
+                  fit: FlexFit.tight,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: _controller,
+                      decoration:
+                          const InputDecoration(hintText: "Write message"),
+                    ),
+                  ),
+                ),
+                Flexible(
+                  fit: FlexFit.tight,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        flutterbluetoothadapter.sendMessage(
+                            _controller.text ?? "no msg",
+                            sendByteByByte: false);
+//                        flutterbluetoothadapter.sendMessage(".",
+//                            sendByteByByte: true);
+                        _controller.text = "";
+                      },
+                      child: const Text('SEND'),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
+  }
+
+  _createDevices() {
+    if (devices.isEmpty) {
+      return [
+        const Center(
+          child: Text("No Paired Devices listed..."),
+        )
+      ];
+    }
+    List<Widget> deviceList = [];
+    devices.forEach((element) {
+      deviceList.add(
+        InkWell(
+          key: UniqueKey(),
+          onTap: () {
+            flutterbluetoothadapter.startClient(devices.indexOf(element), true);
+          },
+          child: Container(
+            padding: const EdgeInsets.all(4),
+            decoration: BoxDecoration(border: Border.all()),
+            child: Text(
+              element.name.toString(),
+              style: const TextStyle(fontSize: 18),
+            ),
+          ),
+        ),
+      );
+    });
+    return deviceList;
   }
 }
